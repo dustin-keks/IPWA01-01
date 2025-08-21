c63c2e214073ed5dc9ae28b61ac09da1d9b0c626
dustin <dustin.vetter@icloud.com>
2025-08-21 22:50:18 +0200

    add bootstrap navbar with logo

---
+++ fallstudie/src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img
+        src="/logo.png"
+        alt="Logo CO₂-Footprint Tracker"
+        width="100"
+        height="100"
+        class="d-inline-block align-text-top"
+      />
+    </a>
+  </div>
+</nav>
