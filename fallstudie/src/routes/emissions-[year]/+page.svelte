b236e218054bd480737996cb0cac0fae5686e05b
dustin <dustin.vetter@icloud.com>
2025-08-26 21:28:41 +0200

    add filter for table

--- fallstudie/src/routes/emissions-[year]/+page.svelte
+++ fallstudie/src/routes/emissions-[year]/+page.svelte
@@ -1,24 +1,57 @@
 <script>
-  export let data;
+  // load json files through +page.js
+  const { data } = $props();
+
+  // create $state rune
+  let sortedRows = $state([...data.rows]);
+
+  // filter table
+  let asc = true;
+
+  function sortByEmission() {
+    sortedRows = [...sortedRows].sort((a, b) => {
+      const result = a.emission - b.emission;
+      return asc ? result : -result;
+    });
+    asc = !asc;
+  }
+
+  function sortByCountry() {
+    sortedRows = [...sortedRows].sort((a, b) => {
+      const result = a.country
+        .toLowerCase()
+        .localeCompare(b.country.toLowerCase());
+      return asc ? result : -result;
+    });
+    asc = !asc;
+  }
+
+  function sortByCompany() {
+    sortedRows = [...sortedRows].sort((a, b) => {
+      const result = a.company
+        .toLowerCase()
+        .localeCompare(b.company.toLowerCase());
+      return asc ? result : -result;
+    });
+    asc = !asc;
+  }
 </script>
 
-<h1>CO₂-Emissionen aus {data.year}</h1>
+<h2>Daten für {data.year}</h2>
 
 <div class="table-responsive">
   <table class="table">
     <caption>Fiktive Daten</caption>
     <thead>
       <tr>
-        <th scope="col">Rang</th>
-        <th scope="col">CO₂-Emissionsdaten</th>
-        <th scope="col">Land</th>
-        <th scope="col">Unternehmen</th>
+        <th scope="col" onclick={sortByEmission}>CO₂-Emissionsdaten</th>
+        <th scope="col" onclick={sortByCountry}>Land</th>
+        <th scope="col" onclick={sortByCompany}>Unternehmen</th>
       </tr>
     </thead>
     <tbody>
-      {#each data.rows as row, i}
+      {#each sortedRows as row}
         <tr>
-          <th scope="row">{i + 1}</th>
           <td>{row.emission}</td>
           <td>{row.country}</td>
           <td>{row.company}</td>
