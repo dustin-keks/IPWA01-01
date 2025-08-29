b3747d85e3cf4e9b1ba5e99ea66c78b817b526e0
dustin <dustin.vetter@icloud.com>
2025-08-29 10:46:14 +0200

    upd menu with local links

--- fallstudie/src/lib/components/NavLocalLinks.svelte
+++ fallstudie/src/lib/components/NavLocalLinks.svelte
@@ -1,22 +1,19 @@
+<script>
+  import { page } from "$app/state";
+  const years = [2025, 2024, 2023, 2022, 2021];
+</script>
+
 <nav class="list-group list-group-flush">
-  <a
-    href="#"
-    class="list-group-item list-group-item-action active"
-    aria-current="true">CO₂-Footprint 2025</a
-  >
-  <a href="#" class="list-group-item list-group-item-action"
-    >CO₂-Footprint 2024</a
-  >
-  <a href="#" class="list-group-item list-group-item-action"
-    >CO₂-Footprint 2023</a
-  >
-  <a href="#" class="list-group-item list-group-item-action"
-    >CO₂-Footprint 2022</a
-  >
-  <a href="#" class="list-group-item list-group-item-action"
-    >CO₂-Footprint 2021</a
-  >
-  <a href="#" class="list-group-item list-group-item-action"
-    >CO₂-Footprint 2020</a
-  >
+  {#each years as year}
+    <a
+      href="/emissions-{year}"
+      class="list-group-item list-group-item-action {page.url.pathname ===
+      `/emissions-${year}`
+        ? 'active'
+        : ''}"
+      aria-current={page.url.pathname === `/emissions-${year}`}
+    >
+      CO₂-Footprint {year}
+    </a>
+  {/each}
 </nav>
